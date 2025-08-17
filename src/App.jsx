15f6ca5ab792c4d18e14c3b1de9035375bca7e28
ArchismanBhaumik <archismanbhaumik786@gmail.com>
2025-08-17 21:52:32 +0530

    aligned items

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ function App() {
   const [ color ,setColor ] = useState('red');
   return (
 
-    <div className='h-screen w-screen duration-200' style={{ backgroundColor: color }}>
+    <div className='h-screen w-screen duration-200' style={{ backgroundColor: color, alignItems: 'center', display: 'flex', flexDirection: 'column' }}>
       <h1 className='text-4xl text-center pt-10'>{color.toString()}</h1>
-       <div className='fixed bottom-12 flex flex-wrap justify-center gap-3 rounded-2xl bg-white p-4 shadow-lg ' style={{left:'25%'}}>
+       <div className='fixed bottom-12 flex flex-wrap justify-center gap-3 rounded-2xl bg-white p-4  shadow-lg m-4'>
               <button className='py-2 px-4 rounded h-10 w-30' style={{ backgroundColor: 'red' }} onClick={()=>setColor('red')}>Red</button>
               <button className='py-2 px-4 rounded h-10 w-30 ' style={{ backgroundColor: 'blue' }} onClick={()=>setColor('blue')}>Blue</button>
               <button className='py-2 px-4 rounded h-10 w-30 ' style={{ backgroundColor: 'green' }} onClick={()=>setColor('green')}>Green</button>
