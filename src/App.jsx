ff1d852040b3d8cb95de390a34703d0b70743b3c
ArchismanBhaumik <archismanbhaumik786@gmail.com>
2025-08-19 01:27:29 +0530

    added new functionality to edit colors

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,18 @@ function App() {
     setColors(tempColors);
     setColor(tempColors[0]); // set initial color
   }, []);
+
+  function changeColor(index){
+     let col = prompt(`Enter color ${index + 1}:`);
+      if (col) {
+        setColors(prev => {
+          const updated = [...prev];
+          updated[index] = col;
+          return updated;
+        });
+        setColor(col); // also update background if needed
+      }
+  }
   
   return (
     <div
@@ -30,6 +42,7 @@ function App() {
           className="py-2 px-4 rounded h-10 w-30"
           style={{ backgroundColor: colors[0] }}
           onClick={() => setColor(colors[0])}
+          onDoubleClick={() => changeColor(0)}
         >
           {colors[0]}
         </button>
@@ -37,6 +50,7 @@ function App() {
           className="py-2 px-4 rounded h-10 w-30 "
           style={{ backgroundColor: colors[1] }}
           onClick={() => setColor(colors[1])}
+          onDoubleClick={() => changeColor(1)}
         >
           {colors[1]}
         </button>
@@ -44,6 +58,7 @@ function App() {
           className="py-2 px-4 rounded h-10 w-30 "
           style={{ backgroundColor: colors[2] }}
           onClick={() => setColor(colors[2])}
+          onDoubleClick={() => changeColor(2)}
         >
           {colors[2]}
         </button>
@@ -51,6 +66,7 @@ function App() {
           className="py-2 px-4 rounded h-10 w-30"
           style={{ backgroundColor: colors[3] }}
           onClick={() => setColor(colors[3])}
+          onDoubleClick={() => changeColor(3)}
         >
           {colors[3]}
         </button>
@@ -58,6 +74,7 @@ function App() {
           className="py-2 px-4 rounded h-10 w-30 "
           style={{ backgroundColor: colors[4] }}
           onClick={() => setColor(colors[4])}
+          onDoubleClick={() => changeColor(4)}
         >
           {colors[4]}
         </button>
@@ -65,6 +82,7 @@ function App() {
           className="py-2 px-4 rounded h-10 w-30 "
           style={{ backgroundColor: colors[5] }}
           onClick={() => setColor(colors[5])}
+          onDoubleClick={() => changeColor(5)}
         >
           {colors[5]}
         </button>
